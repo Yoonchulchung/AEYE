6bdec1ce77f5de34d5b4d61055cafe0c6520bf97
yoonchul <yoonchul005@gmail.com>
2025-10-19 20:09:09 +0900

    chore : boot loader 변경

--- AEYE_AI/app/AEYE/application/boot_loader.py
+++ AEYE_AI/app/AEYE/application/boot_loader.py
@@ -1,18 +1,14 @@
 from AEYE.application.AI.inference import InferenceGPU
 from AEYE.application.AI.loader import GPUModelLoader
 from AEYE.application.AI.models.llm import shutdown_llm
-from AEYE.application.AI.registry import llm_register, vision_register, vlm_register
+from AEYE.application.AI.registry import llm_register, vision_register
 from AEYE.application.logger import AEYE_log
 from AEYE.application.process import Process
 from AEYE.application.registry import get_cfg
 from AEYE_langchain.langchain import init_langchain
 from AEYE_langchain.search import AEYE_langchain_search
 
-llm_model = None
-vision_model = None
-
 async def bootstrap():
-    global llm_model, vision_model
     cfg = get_cfg()
 
     init_langchain()
@@ -22,15 +18,13 @@ async def bootstrap():
         "llm_register" : llm_register,
     }
     model_loader = GPUModelLoader(cfg, registry, AEYE_log)
-    aeye_inference = InferenceGPU(model_loader, cfg, AEYE_langchain_search.get_instance())
-    
-    gpu = Process(cfg, aeye_inference, AEYE_log)
+    inference_gpu = InferenceGPU(model_loader, cfg, AEYE_langchain_search.get_instance())
+    gpu = Process(cfg, inference_gpu, AEYE_log)
 
     return gpu
     
     
 async def shutdown():
-    global llm_model, vision_model
     
     shutdown_llm()
     
