bba61175350cf3e0d425e86b009369bcc1e24124
yoonchul <yoonchul005@gmail.com>
2025-10-23 01:42:53 +0900

    feat : 의사 진단 API 추가

--- AEYE_BE/diagnosis/urls.py
+++ AEYE_BE/diagnosis/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 from rest_framework.routers import DefaultRouter
 
 from diagnosis.api.ai import DiagnosisAIViewSet
+from diagnosis.api.doctor import DiagnosisDoctorViewSet
 
 app_name = 'dianosis'
 
 router = DefaultRouter()
-router.register(r'', DiagnosisAIViewSet, basename='diagnosis')
+router.register(r'ai', DiagnosisAIViewSet, basename='diagnosis AI')
+router.register(r'doctor', DiagnosisDoctorViewSet, basename='diagnosis Doctor')
 
 urlpatterns = router.urls
