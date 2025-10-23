e8ae5d7ba94a12c8de6ca5aadb9fd079fecad975
yoonchul <yoonchul005@gmail.com>
2025-10-23 21:28:15 +0900

    feat : ai api 추가

--- AEYE_BE/ai/urls.py
+++ AEYE_BE/ai/urls.py
@@ -1,8 +1,11 @@
 from rest_framework.routers import DefaultRouter
 
+from .views import AIVersionViewSet
 
-app_name = 'dianose'
+app_name = 'ai'
 
 router = DefaultRouter()
+
+router.register('', AIVersionViewSet, basename='AI Version')
 
 urlpatterns = router.urls
--- AEYE_BE/ai/views.py
+++ AEYE_BE/ai/views.py
@@ -1,3 +1,55 @@
+from django.db import transaction
 from django.shortcuts import render
+from rest_framework import viewsets, mixins, status
+from .serializers import AIVersionSerializer
+from .models import AIVersion
+from rest_framework.response import Response
 
-# Create your views here.
+
+class AIVersionViewSet(mixins.ListModelMixin,
+                       mixins.CreateModelMixin,
+                       viewsets.GenericViewSet):
+    
+    queryset=AIVersion.objects.all()
+    serializer_class = AIVersionSerializer
+
+    def list(self, request, *args, **kwargs):
+        '''
+        모든 AI 모델 정보를 조회합니다.
+        '''
+        
+        qs = self.get_queryset()
+        ser = self.get_serializer(qs, many=True)
+        return Response(ser.data)
+    
+    @transaction.atomic
+    def create(self, request, args, **kwargs):
+        '''
+        새로운 AI 모델 정보를 입력합니다.
+        
+        Request Body:
+        {
+            "oct_model_name": "OCT 모델 이름",
+            "oct_model_weight": "OCT 모델에 사용된 가중치 이름",
+            "oct_probability": "OCT 모델 정확도",
+            "oct_version": "OCT 모델 버젼",
+            "llm_model_name": "llm 모델 이름",
+            "llm_model_weight": "llm 모델 가중치 이름",
+        }
+        '''
+        
+        try:
+            user_serializer = AIVersionSerializer(data=request.data)
+            user_serializer.is_valid(raise_exception=True)
+            user_serializer.save()
+            
+            payload = {
+                "status" : "SUCCESS"
+            }
+        except Exception as e:
+            payload = {
+                "status" : "ERROR",
+                "message" : str(e),
+            }
+
+        return Response(data=payload, status=status.HTTP_202_ACCEPTED)
